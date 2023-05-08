cc8f5356cfad939bfec242d379e9f54f85cc06f9
Nigel Agwah <nigel.agwah@gmail.com>
2023-05-07 22:33:23 -0700

    Changes made to code...unplanned

--- src/customer.py
+++ src/customer.py
@@ -4,8 +4,8 @@ import datetime
 # Variables for names,address, phone, occupation and create a serial number from julian date for profile and demographics
 # We define the initial client account as one block
 
-#clientFirstName = input('First Name: ')
-#clientLastName  = input('Last Name: ')
+clientFirstName = input('First Name: ')
+clientLastName  = input('Last Name: ')
 #clientPhone = input('Phone Number: ')
 #clientAddressStreet = input ('Street: ')
 #clientAddressUnit = input ('Unit/Apt: ')
@@ -14,19 +14,12 @@ import datetime
 #clientAddressZipCode = input ('Zip Code: ')
 #clientOccupation = input('Occupation: ')
 
-def clientAccount():
+def clientAccount(clientFirstName,clientLastName):
     clientFirstName = input('First Name: ')
     clientLastName  = input('Last Name: ')
-    clientPhone = input('Phone Number: ')
-    clientAddressStreet = input ('Street: ')
-    clientAddressUnit = input ('Unit/Apt: ')
-    clientCity = input('City: ')
-    clientAddressState = input ('State: ')
-    clientAddressZipCode = input ('Zip Code: ')
-    clientOccupation = input('Occupation: ')
-
+   
 # For existing accounts we would like to reference information by a key an value system
 # We now create a dictionary to input that information
-print ("Welcome User")
-print(clientAccount(clientFirstName))
-
+clientAccount
+print('Welcome')
+print(str(clientAccount))
